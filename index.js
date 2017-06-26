5c9845b75afce019f8e3927c04586c17f71c18da
kwebster2 <kevin.r.webster@gmail.com>
2017-06-26 09:24:38 -0500

    WIP

---
+++ index.js
@@ -0,0 +1,49 @@
+function Point(x, y){
+  this.x = x,
+  this.y = y
+}
+
+Point.prototype.toString = function() {
+  return `(${this.x}, ${this.y})`
+}
+
+function Side(length){
+  this.length = length;
+}
+
+function Shape(){
+}
+
+Shape.prototype.addToPlane = function(x,y){
+  this.position = new Point(x, y)
+}
+
+Shape.prototype.move = function(x, y){
+  this.position = new Point(x, y)
+}
+
+function Circle(radius){
+  this.radius = radius
+}
+
+Circle.prototype = Object.create(Shape.prototype)
+Circle.prototype.diameter = function(){
+  return this.radius * 2
+}
+
+Circle.prototype.area = function(){
+  return Math.PI * this.radius ** 2
+}
+
+Circle.prototype.circumference = function(){
+  return 2 * Math.PI * this.radius
+}
+
+function Polygon(sides){
+  this.sides = sides
+}
+
+Polygon.prototype = Object.create(Shape.prototype)
+Polygon.prototype.perimeter = function(){
+  return this.sides.reduce((int, next) => int + next.length, 0)
+}
